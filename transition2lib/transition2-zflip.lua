44f05d7eddc8b28a81d2b20dbb6ae0a79356c483
Markus Ranner <markus.ranner@gmail.com>
2017-06-27 09:49:59 +0200

    trying to get the perspectives rights

--- transition2lib/transition2-zflip.lua
+++ transition2lib/transition2-zflip.lua
@@ -2,7 +2,7 @@ local function toRadians(degrees)
     return ((degrees % 360) * math.pi / 180)
 end
 
-local PERSPECTIVE_FACTOR = 0.4
+local MAX_PERSPECTIVE_FACTOR = 0.4
 
 return {
     getStartValue = function(target, params)     
@@ -27,13 +27,14 @@ return {
             
             -- Skew the y coordinates to create perspective            
             local depthOffset = radius * math.sin(radians)            
-            local yOffset = (depthOffset / radius) * (target.height / 2) * (target.height / target.width) * PERSPECTIVE_FACTOR
+            local yOffset = (depthOffset / radius) * target.height/2  * params.perspective
             --print("xOffset = " .. xOffset)
             target.path.y1 = yOffset
             target.path.y2 = -yOffset
             target.path.y3 = yOffset
             target.path.y4 = -yOffset
         else        
+            -- Vertical flip
             local radius = target.height/2
             local yOffset = radius - (radius * math.cos(radians))
             
@@ -44,8 +45,9 @@ return {
             
             -- Skew the x coordinates to create perspective            
             local depthOffset = radius * math.sin(radians)
-            --print("depth offset = " .. depthOffset)
-            local xOffset = (depthOffset / radius) * (target.width / 2) * (target.height / target.width) * PERSPECTIVE_FACTOR
+            --print("depth offset = " .. depthOffset)          
+            
+            local xOffset = (depthOffset / radius) * target.width/2 * params.perspective
             --print("xOffset = " .. xOffset)
             target.path.x1 = xOffset
             target.path.x2 = -xOffset
@@ -56,6 +58,15 @@ return {
  
     getParams = function(target, params)  
         params.horizontalFlip = (params.direction == "horizontal")
+        
+        if ((params.perspective ~= nil) and (params.perspective >= 0) and (params.perspective <= 1)) then
+            params.perspective = MAX_PERSPECTIVE_FACTOR * params.perspective
+        else
+            -- Default perspective
+            params.perspective = MAX_PERSPECTIVE_FACTOR * 0.5
+        end
+        print (params.perspective)
+        return params
     end,
 
     cancelWhen = function(target, params)
