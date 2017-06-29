4c5366c001e9ad2096f32f58b10702e06c46899d
Markus Ranner <markus.ranner@gmail.com>
2017-06-29 14:12:22 +0200

    overriding transition.to() function

--- transition2lib/transition2-to.lua
+++ transition2lib/transition2-to.lua
@@ -7,8 +7,6 @@ Markus Ranner 2017
 --]]
 
 --[[
-size (optional)
-Number. Applies only if the target is a TextObject created via display.newText(). This will transition the font size of the text object.
 x1, y1, x2, y2, x3, y3, x4, y4 (optional)
 Numbers. Applies only if the target is a RectPath, applicable to a ShapeObject. These properties control the quadrilateral distortion of the target.
 [filterParameter] (optional)
@@ -27,6 +25,13 @@ return {
             local propName = SIMPLE_PROPS[i]
             if (params[propName] ~= nil) then
                 startValue[propName] = displayObject[propName] or 0
+            end
+        end
+        
+        -- For rect paths, the (x1,y1)...(x4,y4) are also possible to change
+        if (utils.isRectPath(displayObject)) then
+            for i = 1, 4 do
+                
             end
         end
         
