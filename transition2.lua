caa7e74356d6ac971d12bf4ddf134b6859d8ef23
Markus Ranner <markus.ranner@gmail.com>
2017-07-03 20:14:50 +0200

    removed unfinished transition

--- transition2.lua
+++ transition2.lua
@@ -22,8 +22,7 @@ local transition2 = createTransition2({
     moveSine = require("transition2lib.transition2-moveSine"),   
     moveBungy = require("transition2lib.transition2-moveBungy"),   
     zRotate = require("transition2lib.transition2-zRotate"),
-    waterBalloon = require("transition2lib.transition2-waterBalloon"),
-    oceanWaves = require("transition2lib.transition2-oceanWaves"),
+    waterBalloon = require("transition2lib.transition2-waterBalloon"),    
     
     -- Convenience functions (specialized versions of transitions)
     glow = require("transition2lib.transition2-glow"),  
