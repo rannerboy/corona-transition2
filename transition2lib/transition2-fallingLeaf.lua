e7dc8275341706ea4cf1cbe870d64bedc313970f
Markus Ranner <markus.ranner@gmail.com>
2017-07-16 23:09:14 +0200

    falling leafs

--- transition2lib/transition2-fallingLeaf.lua
+++ transition2lib/transition2-fallingLeaf.lua
@@ -4,31 +4,36 @@ Falling leaf
 TODO: Add comments and example 
 
 transition.fallingLeaf(displayObject, {
-    time = 2000, -- Default = 1000. The time for one vertical iteration.
+    time = 2000, -- Default = 1500. The time for one vertical iteration.
     deltaY = 100, -- Default = 200
     deltaX = 150, -- Default = 200
     randomHorizontalDirection = true,
     disableSlowStart = true, -- Default = true. A slow start means that deltaX/Y will be gradually increased from 0 to specified param values
+    rotate = false, -- Default = true. Applies rotation to the object.
+    zRotate = false, -- Default = true. Applies zRotate transition with shading.
 })
 
 Markus Ranner 2017
 
 --]]
 return function(transition2)
-    return function (obj, params)        
-        -- FIXME: Add randomization to movement
+    return function (obj, params)
+        
+        -- FIXME: Handle all onX functions and pass them on to other transition functions
+        -- FIXME: Handle cancelWhen function and pass it on
         
         local SLOW_START_MIN_FACTOR = 0.5
         local SLOW_START_INCREASE_FACTOR = 1.2
         
-        -- FIXME: Remove hard coded params
+        -- Params decoding
         local maxRadiusY = params.deltaY or 200
         local maxRadiusX = params.deltaX or 200
         local radiusY = params.disableSlowStart and maxRadiusY or (SLOW_START_MIN_FACTOR * maxRadiusY)
-        local radiusX = params.disableSlowStart and maxRadiusX or (SLOW_START_MIN_FACTOR * maxRadiusX)
-        local maxTime = (params.time or 1000)
-        local time = maxTime --params.disableSlowStart and maxTime or (0.5 * maxTime)
+        local radiusX = params.disableSlowStart and maxRadiusX or (SLOW_START_MIN_FACTOR * maxRadiusX)        
+        local time = (params.time or 1500)
         local randomHorizontalDirection = params.randomHorizontalDirection or false
+        local rotationEnabled = (params.rotate ~= false)
+        local zRotationEnabled = (params.zRotate ~= false)
         
         -- State variables
         local verticalDirection = "down"        
@@ -38,8 +43,8 @@ return function(transition2)
         local moveVertical
         moveVertical = function()
             transition2.moveSine(obj, {
-                time = time,
-                radiusY = (verticalDirection == "down") and radiusY or (radiusY * 0.25),
+                time = (verticalDirection == "down") and time or time/2,
+                radiusY = (verticalDirection == "down") and radiusY or (radiusY * math.random(0, 10) / 100),
                 deltaDegreesY = 180,
                 startDegreesY = (verticalDirection == "down") and 270 or 90,
                 iterations = 1,
@@ -69,7 +74,7 @@ return function(transition2)
         local moveHorizontal
         moveHorizontal = function()
             transition2.moveSine(obj, {
-                time = time * 2 * math.random(9, 11) / 10,
+                time = time * 2 * math.random(5, 20) / 10,
                 radiusX = radiusX,
                 deltaDegreesX = 180,
                 startDegreesX = (horizontalDirection == "right") and 270 or 90,
@@ -98,5 +103,32 @@ return function(transition2)
         
         moveVertical()
         moveHorizontal()
+        
+        if (rotationEnabled) then
+            transition.to(obj, {
+                time = time,
+                onIterationStart = function(obj, params)
+                    params.rotation = obj.rotation + math.random(-360, 360)
+                end,
+                iterations = 0,
+                reverse = true,
+                transition = easing.inOutSine,
+                recalculateOnIteration = true,
+            })                    
+        end
+        
+        -- Apply zRotate
+        if (zRotationEnabled) then
+            transition.zRotate(obj, {
+                time = time,                 
+                reverse = true,
+                iterations = 0,
+                shading = true, -- FIXME: Make it possible to disable shading
+                onIterationStart = function(obj, params) 
+                    params.degrees = math.random(-360, 360)
+                end,
+                recalculateOnIteration = true,
+            })
+        end
     end
 end
