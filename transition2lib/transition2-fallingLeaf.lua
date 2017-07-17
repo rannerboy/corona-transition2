f4dc17e8c20ae5886c19e419bbeff590dddf738e
Markus Ranner <markus.ranner@gmail.com>
2017-07-17 20:52:39 +0200

    tweaking fallingLeaf

--- transition2lib/transition2-fallingLeaf.lua
+++ transition2lib/transition2-fallingLeaf.lua
@@ -5,8 +5,8 @@ TODO: Add comments and example
 
 transition.fallingLeaf(displayObject, {
     speed = 0.25, -- A value between 0-1. Default = 0.5.
+    windIntensity = 0.75, -- A value between 0-1. Default = 0.5.
     
-    deltaX = 150, -- Default = 200
     randomHorizontalDirection = true,
     rotate = false, -- Default = true. Applies rotation to the object.
     zRotate = false, -- Default = true. Applies zRotate transition with shading.
@@ -17,7 +17,7 @@ Markus Ranner 2017
 --]]
 
 local DEFAULT_SPEED = 0.5
-local DEFAULT_TIME = 1500
+local DEFAULT_WIND_INTENSITY = 0.5
 
 local function getValidSpeed(speed)
     if (speed == nil) then
@@ -35,21 +35,30 @@ local function getBaseDeltaY(speed)
     local MIN_DELTA_Y = 25
     local MAX_DELTA_Y = 400
     
-    local targetDeltaY = ((MAX_DELTA_Y - MIN_DELTA_Y) * speed) + MIN_DELTA_Y
-    
-    print("targetDeltaY = " .. targetDeltaY)
-    
-    return targetDeltaY
+    local baseDeltaY = ((MAX_DELTA_Y - MIN_DELTA_Y) * speed) + MIN_DELTA_Y
+        
+    return baseDeltaY
 end
 
 local function getBaseDeltaX(windIntensity)
-    -- FIXME
-    return 100
+    local MIN_DELTA_X = 25
+    local MAX_DELTA_X = 300
+    
+    local baseDeltaX = ((MAX_DELTA_X - MIN_DELTA_X) * windIntensity) + MIN_DELTA_X
+    
+    return baseDeltaX
 end
 
 local function getValidWindIntensity(windIntensity)
-    -- FIXME
-    return 0.5
+    if (windIntensity == nil) then
+        return DEFAULT_WIND_INTENSITY
+    elseif (windIntensity < 0) then
+        return 0
+    elseif(windIntensity > 1) then
+        return 1
+    else
+        return windIntensity
+    end   
 end
 
 local function getTime(speed)
@@ -57,8 +66,7 @@ local function getTime(speed)
     local MAX_TIME = 3000
     
     -- Time will increase as speed increases
-    local time = ((MAX_TIME - MIN_TIME) * speed) + MIN_TIME
-    print("time = " .. time)
+    local time = ((MAX_TIME - MIN_TIME) * speed) + MIN_TIME    
     
     return time
 end
@@ -97,11 +105,12 @@ return function(transition2)
             end
             
             transition2.moveSine(obj, {
-                time = (verticalDirection == "down") and time or time/3,
+                -- FIXME: This time calculation doesn't feel very good
+                time = (verticalDirection == "down") and time or time/2.5,
                 radiusY = radiusY,
                 deltaDegreesY = 180,
                 startDegreesY = (verticalDirection == "down") and 270 or 90,
-                iterations = 1,                
+                iterations = 1,
                 onIterationComplete = function(obj, params)                     
                     -- Flip direction and start new moveSine transition
                     verticalDirection = (verticalDirection == "down") and "up" or "down"                    
@@ -113,17 +122,18 @@ return function(transition2)
         local moveHorizontal
         moveHorizontal = function()
             
-            local radiusX = baseDeltaX
+            -- Randomize radiusX quite a lot
+            local radiusX = baseDeltaX * (math.random(50, 150) / 100)            
             
             transition2.moveSine(obj, {
-                time = time * 2 * math.random(5, 20) / 10,
+                time = time * 1.5 * math.random(5, 15) / 10,
+                --time = time,
                 radiusX = radiusX,
                 deltaDegreesX = 180,
                 startDegreesX = (horizontalDirection == "right") and 270 or 90,
                 iterations = 1,                
                 onIterationComplete = function(obj, params)                    
-                    -- FIXME: Add randomization to radiusX
-                    
+                    -- Calculate new direction of horizontal movement
                     if (randomHorizontalDirection) then
                         horizontalDirection = (math.random(1, 2) == 1) and "right" or "left"
                     else
@@ -138,6 +148,7 @@ return function(transition2)
         moveVertical()
         moveHorizontal()
         
+        -- FIXME: rotation should depend on windIntensity
         if (rotationEnabled) then
             transition.to(obj, {
                 time = time,
@@ -151,6 +162,7 @@ return function(transition2)
             })                    
         end
         
+        -- FIXME: zRotate speed should depend on windIntensity
         -- Apply zRotate
         if (zRotationEnabled) then
             -- FIXME: Allow a separate zRotate params object to be passed in to fallingLeaf() to customize the zRotation and overwrite default settings.
